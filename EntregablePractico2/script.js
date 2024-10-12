44778634dd399b0ff0368087fb2dcaca9d272ef3
Ezequiel <echimartinez@gmail.com>
2024-10-12 12:54:18 -0300

    agrego loading

---
+++ EntregablePractico2/script.js
@@ -0,0 +1,21 @@
+const percentageElement = document.getElementById('percentage');
+const progressBar = document.querySelector('.progress');
+let percentage = 0;
+
+const interval = setInterval(() => {
+    percentage += 1;
+    percentageElement.textContent = `${percentage}%`;
+    progressBar.style.width = `${percentage}%`;
+    
+    if (percentage >= 100) {
+        clearInterval(interval);
+        
+        // Añadir animación de finalización
+        progressBar.style.animation = 'completeAnimation 0.5s ease forwards';
+        
+        // Redirigir después de la animación
+        setTimeout(() => {
+            window.location.href = 'index.html'; // Cambia a la URL de tu home
+        }, 0); // 500 ms para esperar a que termine la animación
+    }
+}, 50); 
